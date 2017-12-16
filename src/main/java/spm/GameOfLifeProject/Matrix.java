46af4372d169484afe42e861a88ca7d8cf59f876
Biruk Bekele <10180903+brocklife@users.noreply.github.com>
2017-12-16 19:11:33 +0100

    Update Matrix.java

--- src/main/java/spm/GameOfLifeProject/Matrix.java
+++ src/main/java/spm/GameOfLifeProject/Matrix.java
@@ -21,19 +21,19 @@ public class Matrix {
     
     public static void swap_matrices() {
     	int[][] temp = matrix;
-		matrix = tempNext;
-		tempNext = temp;
+	matrix = tempNext;
+        tempNext = temp;
 	}
     public final static int DEAD = 0;
-	public final static int ALIVE = 1;
-	
-	/** A function that counts the alive neighbours of a cell
-	 * 
-	 * @param i row identifier
-	 * @param j column identifier
-	 * @return the number of alive cells around (i,j)
-	 */
-   private int countNoAliveNeighbours(int i, int j) {
+    public final static int ALIVE = 1;
+    
+    /** A function that counts the alive neighbours of a cell
+     * 
+     * @param i row identifier
+     * @param j column identifier
+     * @return the number of alive cells around (i,j)
+     */
+    private int countNoAliveNeighbours(int i, int j) {
 		int result = 0;
 		int prevRow = i - 1, succRow = i + 1, prevColumn = j - 1, succColumn = j + 1;
 
@@ -51,15 +51,15 @@ public class Matrix {
 				+ Matrix.matrix[succRow][j] + Matrix.matrix[succRow][succColumn];
 
 		return result;
-	}
-
-	static int[][] tempNext = new int[GolSkandium.MATRIX_SIZE + 2][GolSkandium.MATRIX_SIZE + 2];
+    }
+    
+    static int[][] tempNext = new int[GolSkandium.MATRIX_SIZE + 2][GolSkandium.MATRIX_SIZE + 2];
     /**
      * 
      * @param interval indicate the start of the row and no of rows
      * @return matrix  the element in position (i,j) in the Matrix board
      */
-	public Interval neighbors2(Interval interval) {
+    public Interval neighbors2(Interval interval) {
 		int m = interval.a;
 		int n = interval.b;
 		for (int i = m; i <= n; i++) {
@@ -89,11 +89,9 @@ public class Matrix {
 				}
 			}
 		}
-
-
-		return interval;
-
-	}
+	    return interval;
+    }
+	
     /**
      * To print the the evoultion 
      * @param interval used to get the start and end of the board 
