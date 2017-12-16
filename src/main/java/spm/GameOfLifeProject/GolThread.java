27d41634408bb59c6e770bfb4956c69c94236897
Biruk Bekele <10180903+brocklife@users.noreply.github.com>
2017-12-16 18:15:59 +0100

    Update GolThread.java

--- src/main/java/spm/GameOfLifeProject/GolThread.java
+++ src/main/java/spm/GameOfLifeProject/GolThread.java
@@ -16,35 +16,32 @@ import java.util.concurrent.CyclicBarrier;
  *
  */
 public class GolThread {
-		
-	 
 	int THREADS; 
 	int matrix_size;
 	public static int ngen;
 	public static long seed ;
-    public static int seedBool;
-			
-
+        public static int seedBool;
+	
 	public GolThread (int tHREADS, int iter,int bOARD) {
 		THREADS = tHREADS;
-	    ngen =iter;
+	        ngen =iter;
 		matrix_size = bOARD;
-		 
 	}
 
 	public void execute() throws Exception {
 		Golboard board = new Golboard(matrix_size);
-        Interval[] bounds = board.splitBoard(THREADS);
-        if(seedBool== 1){
+		Interval[] bounds = board.splitBoard(THREADS);
+		if(seedBool== 1){
 			board.seedintialize(seed);
-        } else {
+		} else {
          	board.Randomintialize();
 		} 
 		 
 		final CyclicBarrier barrier = new CyclicBarrier(THREADS, board::swapplDsil);
 		ExecutorService threadpool = Executors.newFixedThreadPool(THREADS);
 		long init = System.currentTimeMillis();
-        for (int j = 0; j < THREADS; j++) {
+		
+		for (int j = 0; j < THREADS; j++) {
 			threadpool.execute(new WorkerThread(board,bounds[j].a,bounds[j].b ,ngen, barrier));
 		}
 		threadpool.shutdown();
