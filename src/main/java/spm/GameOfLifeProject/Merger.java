c8a04c224bdd73c2978c2b18d0bac06877f38975
Biruk Bekele <10180903+brocklife@users.noreply.github.com>
2017-12-16 19:15:10 +0100

    Update Merger.java

--- src/main/java/spm/GameOfLifeProject/Merger.java
+++ src/main/java/spm/GameOfLifeProject/Merger.java
@@ -26,25 +26,25 @@ public class Merger implements Merge<Interval, Interval> {
      * @param maxGene
      */
     public Merger(int maxGene) {
-        Merger.maxGene = maxGene;
+	Merger.maxGene = maxGene;
     }
     @Override
     public Interval merge(Interval[] arg0) throws Exception {
         int small = 100000000;
         int large = 0;
-		for (int i = 0; i < arg0.length; i++) {
-			for (int ii = 1 ;ii<=maxGene;ii++){
-				if (arg0[i].a< small) {
-					small = arg0[i].a;
-					
-				} if (arg0[i].b> large) {
-					large = arg0[i].b;
-                }
-			} 
-        }
+	    
+	for (int i = 0; i < arg0.length; i++) {
+		for (int ii = 1 ;ii<=maxGene;ii++){
+			if (arg0[i].a< small) {
+			small = arg0[i].a;
+			} if (arg0[i].b> large) {
+				large = arg0[i].b;
+			}
+		} 
+	}
 		
         Matrix.swap_matrices();
         maxGene--;
-       return new Interval(small, large);
+        return new Interval(small, large);
     }
 }
